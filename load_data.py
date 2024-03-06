71a617fb058e279594dfb6ac2d2a5e29797f0a15
Elmir Zeynalov <elmirz@ml9.hpc.uio.no>
2024-03-06 02:14:29 +0100

    class accuracy calculation added

--- load_data.py
+++ load_data.py
@@ -5,6 +5,8 @@ import numpy as np
 import torch
 from PIL import Image
 from torchvision import transforms, models
+from sklearn import metrics
+import torch.nn.functional as F
 
 class ImageDataset(data.Dataset):
     def __init__(self, root_dir, train=True, transform=None):
@@ -140,31 +142,83 @@ def train(model, optimizer, criterion, num_epochs, data_loader):
 def validate(model, criterion, num_epochs, data_loader):
     correct = 0
     total_samples = 0
+    confusion_matrix = np.zeros(shape=(10,10))
+    #accuracies    = np.zeros(shape=num_epochs)
+    accuracies = []
+    labels_list= [0,1,2,3,4,5]
+
+    confusion_matrix = np.zeros((6, 6))
+    class_accuracies = np.zeros(6)
+    class_precisions = np.zeros(6)
+    class_recalls = np.zeros(6)
+    class_f1_scores = np.zeros(6)
+    class_aps = np.zeros(6)
+
+
     print("---Validating---")
     model.eval()
     with torch.no_grad():
         for epoch in range(num_epochs):
+            correct = 0
+            total_samples = 0
+            epoch_labels = []
+            epoch_predictions = []
             for batch in data_loader:
                 inputs = batch['image']
                 labels = batch['label']
-
                 
                 inputs, labels = inputs.to(device), labels.to(device)
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
 
-                #predicted_label  = outputs.max(1, keepdim=True)[1][:,0]
-                #correct += predicted_label.eq(labels).cpu().sum().numpy()
                 predicted_label = outputs.argmax(dim=1)
                 correct += (predicted_label == labels).sum().item()
                 total_samples += labels.size(0)
 
-            print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}')
-    #accuracy = correct / len(data_loader.dataset)
-    accuracy = correct / total_samples
-    print(f'Validation Accuracy: {accuracy * 100:.2f}%')
-    return accuracy
+                confusion_matrix += metrics.confusion_matrix(labels.cpu().numpy(), predicted_label.cpu().numpy(), labels=labels_list)
+                # Append labels and predictions for calculating AP later
+                epoch_labels.extend(labels.cpu().numpy())
+                epoch_predictions.extend(F.softmax(outputs, dim=1).cpu().numpy())
 
+            accuracy = correct/total_samples
+            accuracies.append(accuracy)
+            for class_index in range(6):
+                class_labels = [1 if label == class_index else 0 for label in epoch_labels]
+                class_predictions = [prediction[class_index] for i, prediction in enumerate(epoch_predictions)]
+                
+                #class_accuracies[class_index] = metrics.accuracy_score(class_labels, np.array(class_predictions).round())
+                class_accuracies[class_index] = metrics.accuracy_score(class_labels, np.array(class_predictions).round(), normalize=False)
+                class_precisions[class_index] = metrics.precision_score(class_labels, np.array(class_predictions).round())
+                class_recalls[class_index] = metrics.recall_score(class_labels, np.array(class_predictions).round())
+                class_f1_scores[class_index] = metrics.f1_score(class_labels, np.array(class_predictions).round())
+                class_aps[class_index] = metrics.average_precision_score(class_labels, np.array(class_predictions))
+
+            #print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}')
+            # Print metrics for each class at the end of the epoch
+            print(f'Epoch {epoch + 1}: Accuracy: {accuracy * 100:.2f}%')
+            for class_index in range(6):
+                print(f'  Class {class_index}: Accuracy: {class_accuracies[class_index] / total_samples * 100:.2f}%, Precision: {class_precisions[class_index]:.4f}, Recall: {class_recalls[class_index]:.4f}, F1 Score: {class_f1_scores[class_index]:.4f}, AP: {class_aps[class_index]:.4f}')
+                #print(f'  Class {class_index}: Accuracy: {class_accuracies[class_index] * 100:.2f}%, Precision: {class_precisions[class_index]:.4f}, Recall: {class_recalls[class_index]:.4f}, F1 Score: {class_f1_scores[class_index]:.4f}, AP: {class_aps[class_index]:.4f}')
+
+    #accuracy = correct / len(data_loader.dataset)
+    #accuracy = correct / total_samples
+    #confusion_matrix = confusion_matrix / total_samples
+    mAP = np.mean(class_aps)
+
+    #print(f'Validation Accuracy: {accuracy * 100:.2f}%')
+    #print("\nAccuracies for each epoch:")
+    #for epoch, acc in enumerate(accuracies):
+    #    print(f'Epoch {epoch + 1}: Accuracy: {acc * 100:.2f}%')
+    
+    print("\nAccuracies for each epoch:")
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch + 1}: Accuracy: {accuracies[epoch] * 100:.2f}%')
+
+    print(f'Mean Average Precision (mAP): {mAP:.4f}')
+    print("Confusion Matrix:")
+    print(confusion_matrix)
+
+    return accuracy
 
 def train_resnet18_model(device, train_loader, val_loader):
     model = models.resnet18(pretrained=True)
@@ -182,23 +236,26 @@ def train_resnet18_model(device, train_loader, val_loader):
     validate(model, criterion, num_epochs, val_loader)
 
 
-def create_datasets_and_loaders(root_path, transform=None):
+def create_datasets_and_loaders(root_path, transform=None, seed=50):
+
+    def worker_init_fn(worker_id):
+        torch.manual_seed(seed + worker_id)
+    
     # Training dataset and dataloader
     train_dataset = ImageDataset(root_dir=root_path, train=True, transform = transform)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=False, num_workers=4,worker_init_fn=worker_init_fn)
 
     # Validation dataset and dataloader
     val_dataset = ImageDataset(root_dir=root_path, train=False, transform = transform)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=True, num_workers=4)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4, worker_init_fn=worker_init_fn)
 
     return train_loader, val_loader
-
 
 if __name__ == "__main__":
     root_path = "Images/mandatory1_data/"
 
     #split data into training, validation and test data sets
-    dataset = DataSplitter(root_path)
+    #dataset = DataSplitter(root_path)
     print("Done creating train, validation and test sets...")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -208,8 +265,12 @@ if __name__ == "__main__":
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
+    seed = 42
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
     #data sets and loaders
-    train_loader, val_loader = create_datasets_and_loaders(root_path=root_path, transform=transform)
+    train_loader, val_loader = create_datasets_and_loaders(root_path=root_path, transform=transform, seed=seed)
 
     #training and validation
     train_resnet18_model(device, train_loader, val_loader)
